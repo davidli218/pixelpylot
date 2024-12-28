ab2122f29252f1037f13acf2783f13d3ce1b1f8b
Ri Teii <teiiri@outlook.com>
2024-12-28 03:18:16 +0000

    🐞 Fix missing default argument

--- app/__main__.py
+++ app/__main__.py
@@ -12,7 +12,7 @@ def main():
     subparsers = parser.add_subparsers(dest='command', help='sub-command help')
 
     sony_clip_rename_parser = subparsers.add_parser('rename-sony-clip', help='Batch rename Sony clips')
-    sony_clip_rename_parser.add_argument('work_dest', default='.', help='Directory or file to process.')
+    sony_clip_rename_parser.add_argument('work_dest', nargs='?', default='.', help='Directory or file to process.')
     sony_clip_rename_parser.add_argument('-d', '--dry', action='store_true', help='Dry run.')
     sony_clip_rename_parser.add_argument('-s', '--strict', action='store_true', help='Strict mode.')
 
